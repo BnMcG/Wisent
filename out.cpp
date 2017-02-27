8ce41079aa71c61c0cac427893f53c500f3b0dd5
Ben Magee <bmagee@cogative.com>
2017-02-27 16:43:16 +0000

    final

---
+++ out.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+std::cout <<  14 << std::endl; 
+std::cout <<  0 << std::endl; 
+std::cout <<  20.5 << std::endl; 
+std::cout <<  1 << std::endl; 
+std::cout <<  0 << std::endl; 
+std::cout <<  5 << std::endl; 
+std::cout <<  3.33333 << std::endl; 
+std::cout <<  11 << std::endl; 
+std::cout <<  -3 << std::endl; 
+std::cout <<  28 << std::endl; 
+}
